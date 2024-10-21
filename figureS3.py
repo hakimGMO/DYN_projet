f60f9f11214cd880c33fee78927a8ffa9fc9926e
Arthur <arthurbarreau.ab@gmail.com>
2024-10-21 15:39:50 +0000

    Message

--- figureS3.py
+++ figureS3.py
@@ -9,8 +9,15 @@ k1 = 0.222
 n = 2
 p = 5
 
-def comK(K, S):
-    return ak + (bk * K**n) / (k0**n + K**n) - K / (1 + K + S)
+def dK_dt(K, S):
+    return ak + (bk * K**n) / (k0**n + K**n) - (K / (1 + K + S))
 
-def comS(K, S):
-    return bs / (1 + (K/k1)**p) - S / (1 + K + S)
+def dS_dt(K, S):
+    return bs / (1 + (K/k1)**p) - (S / (1 + K + S))
+
+
+for K in np.arange(0, 8, 0.0001):
+    for S in np.arange(0,6,0.00001):
+        if dK_dt(K,S) == 0:
+            print(K,S)
+    
