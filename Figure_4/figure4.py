88c8aaa4b169987543a01639d67f36816e9a8be2
Arthur <arthurbarreau.ab@gmail.com>
2024-11-12 15:06:55 +0000

    dernière version figure 4

--- Figure_4/figure4.py
+++ Figure_4/figure4.py
@@ -44,37 +44,39 @@ initial_guesses = [
     [0.12, 3.8]
 ]
 
-def plot_nullclines_and_trajectory(k, s, t_end, dt, mult_K):
+def plot_nullclines_and_trajectory(k, s, t_end, dt, mult_K): # Affichage de la figure 4a, avec nullcline, champs de vecteur et simulation
     
     k_vals = np.linspace(0, 0.45*mult_K, 1000) # Prend 1000 valeur entre 0 et 4.5
     s_vals = np.linspace(0, 8, 1000)
     
     k_grid, s_grid = np.meshgrid(k_vals / mult_K, s_vals) # Diviser par mult_K pour que le calcul soit fait dans les bonnes unités (4.5 --> 0.45)
     
-    dk_dt = ComK(k_grid, s_grid, BK)
+    dk_dt = ComK(k_grid, s_grid, BK) # Tableau numpy de l'ensemble des donnes de dk et ds par rapport a dt
     ds_dt = ComS(k_grid, s_grid, BS)
 
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(8, 6)) # Plot la figure 
     plt.contour(k_grid * mult_K, s_grid, dk_dt, levels=[0], colors='blue', linewidths=2)
     plt.contour(k_grid * mult_K, s_grid, ds_dt, levels=[0], colors='green', linewidths=2)
 
-    k_vector = np.linspace(0, 0.45*mult_K, 10)
+    # Champs de vecteur 
+    k_vector = np.linspace(0, 0.45*mult_K, 10) # Cette fois ci que 10 pour ne pas trop avoir de fleche pour un plus belle visibilité 
     s_vector = np.linspace(0, 8, 10)
     k_vec, s_vec = np.meshgrid(k_vector / mult_K, s_vector)
     dk_dt_vec = ComK(k_vec, s_vec, BK)
     ds_dt_vec = ComS(k_vec, s_vec, BS)
     plt.quiver(k_vec * mult_K, s_vec, dk_dt_vec * mult_K, ds_dt_vec, scale=2, color='gray', scale_units='xy', angles='xy', width=0.004)
 
-    # Ajouter plusieurs simulations en rose transparent
+    # Afficher des simulations des mouvements de ComK et ComS dans le temps
+    
+    plt.plot(k * mult_K, s, color='purple', linewidth=1)# Une trace qui resort en violet 
+    
+    # Plusieurs simulations en rose transparentes en fond 
     num_simulations = 10
     for _ in range(num_simulations):
         k_sim, s_sim, _ = simulate_system(BK, BS, K0, S0, T_END, DT)
         plt.plot(k_sim * mult_K, s_sim, color='pink', alpha=0.3, linewidth=1)
 
-    # Tracer la trajectoire principale en violet
-    plt.plot(k * mult_K, s, color='purple', linewidth=1)
-
-    
+   
     # Calcule quand ComK et ComS sont egaux dans les environs des trois positions donner (guess)
     intersection_list = []
     for guess in initial_guesses:
@@ -83,15 +85,18 @@ def plot_nullclines_and_trajectory(k, s, t_end, dt, mult_K):
     
     
     for intersection in intersection_list:
-        plt.plot(intersection[0] * 10, intersection[1], 'ro')
+        plt.plot(intersection[0] * 10, intersection[1], 'ro') # Affiche les points en rouge
 
+    # Parametre d'affichage 
     plt.xlabel('ComK cocnentration (échelle x10)')
     plt.ylabel('ComS cocnentration')
     plt.ylim(0, 8)
     plt.savefig('figure_4a', dpi=300)
     plt.show()
 
-def plot_time_series(k, s, t_end, dt):
+
+
+def plot_time_series(k, s, t_end, dt): # Affichage de la figure 4b, une simulation dans le temps du comportement de ComK et ComS
     plt.figure(figsize=(8, 6))
     plt.plot(np.arange(0, t_end, dt), k * 10, label='ComK Concentration', color='blue', linewidth=2)
     plt.plot(np.arange(0, t_end, dt), s, label='ComS Concentration', color='green', linewidth=2)
@@ -100,6 +105,9 @@ def plot_time_series(k, s, t_end, dt):
     plt.legend()
     plt.savefig('figure_4b.png', dpi=300)
     plt.show()
+
+###########################################################################
+# Main 
 
 # Paramètres de la simulation
 BK = 0.08
